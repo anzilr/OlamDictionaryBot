61e3ba6e281d59308e248a1446db33e4bedd1ca0
AnzilR <anzilraheem@gmail.com>
2021-09-23 11:33:02 +0530

    Updated

--- bot/plugins/cmd/help.py
+++ bot/plugins/cmd/help.py
@@ -1,9 +1,28 @@
 from ...olambot import OlamBot, filters
-from .message import DEV, START, SOURCE
+from urllib.parse import quote
+from pyrogram.types import *
 
 
 @OlamBot.on_message(filters.command(["help", "h"]))
 async def onHelp(_, msg):
+    btext = 't.me/share/url?url=' + quote("Hey, Check out @olamdictionarybot for English-Malayalam word meanings.")
     await msg.reply(
-        f"Hy <b>{msg.from_user.first_name},{START}{DEV}{SOURCE}</b>"
+        f'''
+▪️ Send <i>/start</i> to wake the bot up.\n
+▪️ <i>/help</i> will show you a message with available commands.\n
+▪️ In groups, sending <i>/olam</i> along with your query will give you the results.\n
+▪️ When you send queries to the bot in private chat, just send the words only. No need for dumb commands.\n
+▪️ For use in inline mode, type @olamdictionarybot in the field, tap the space bar, and start typing your word.  The result will be shown above. Just tap it, and there you go!\n
+\n
+<b>Click 🤖 Updates button below 👇🏻 for more info.</b>\n
+<b>Click 🔍 Search button to start searching.</b>
+        ''',
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton(text="🔍 Search", switch_inline_query_current_chat=""),
+                 InlineKeyboardButton("⤴️ Share", url=btext),
+                 InlineKeyboardButton("🤖 Updates", url="https://telegra.ph/Olam-Dictionary-Bot-09-22")]
+            ]
     )
+    )
+
--- bot/plugins/cmd/start.py
+++ bot/plugins/cmd/start.py
@@ -1,10 +1,32 @@
 from pyrogram.filters import command
 from ...olambot import OlamBot, filters
-from .message import DEV, START, SOURCE
+from urllib.parse import quote
+from pyrogram.types import *
 
 
 @OlamBot.on_message(filters.command("start"))
 async def onStart(_, msg):
+    btext = 't.me/share/url?url=' + quote("Hey, Check out @olamdictionarybot for English-Malayalam word meanings.")
     await msg.reply(
-        f"Hy <b>{msg.from_user.first_name},{START}{DEV}{SOURCE}</b>"
+        '''
+[Olam](https://olam.in/) is a growing, free and open, crowdsourced English-Malayalam dictionary with over 200,000 entries. 
+You can use this bot to get your Malayalam meaning from the [olam](https://olam.in/) database. 
+Click 👉🏻 /help to know how to use.
+
+രണ്ട് ലക്ഷത്തില്‍പ്പരം വാക്കുകളും വ്യാഖ്യാനങ്ങളുമായി അനുദിനം വളര്‍ന്നുകൊണ്ടിരിക്കുന്ന ഒരു ഓപ്പണ്‍ സോഴ്സ് ഇംഗ്ലീഷ് - മലയാളം നിഘണ്ടുവാണ് [ഓളം](https://olam.in/).
+[ഓളം](https://olam.in/) ഡേറ്റാബേസില്‍ നിന്നും നിങ്ങള്‍ക്കാവശ്യമുള്ള ഇംഗ്ലീഷ് വാക്കുകളുടെ അര്‍ത്ഥമറിയാനായി ഈ ബോട്ടിനെ ഉപയോഗിക്കാം. 
+സഹായത്തിനായി 👉🏻 /help അമര്‍ത്തുക. 
+
+Check out the code in [GitHub](https://github.com/anzilr/OlamDictionaryBot)
+For anything about the bot, contact [Anzil R](https://t.me/anzilr)
+Here is the original [code](https://github.com/AbhijithNT/OlamTelegramBot)
+ ''',
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton(text="🔍 Search", switch_inline_query_current_chat=""),
+                 InlineKeyboardButton("⤴️ Share", url=btext),
+                 InlineKeyboardButton("🤖 Updates", url="https://telegra.ph/Olam-Dictionary-Bot-09-22")]
+
+            ]
+        )
     )

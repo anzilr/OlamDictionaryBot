431c29fe71d69516c60cf5603e51513d308c8acf
Anzil R <anzilraheem@gmail.com>
2021-10-06 23:38:42 +0530

    added malayalam-english dictionary

--- bot/helpers/malayalam_dict.py
+++ bot/helpers/malayalam_dict.py
@@ -1,15 +1,21 @@
 import csv
 import json
+from pyuca import Collator
 def malayalamDict(word):
 
     parts_of_speech_json = json.load(open("data/parts-of-speech.json"))
     with open("data/olam-enml.csv", "r") as f:
         r = csv.reader(f, delimiter='\t')
-        dictionary = [row for row in r if row[1].lower().startswith(word.lower())] #append rows from csv file based on input word
-        mal_definitions = [row[-1] for row in dictionary if row[1].lower().startswith(word.lower())] #append definitions from dictionary
-        eng_word = [row[1] for row in dictionary if row[1].lower().startswith(word.lower())] #append english words from dictionary
-        parts_of_speech = [row[-2] for row in dictionary if row[1].lower().startswith(word.lower())] #append parts of speech from dictionary
-        parts_of_speech_defs = [parts_of_speech_json[parts_of_speech[p]]['en'] for p in range(0, len(parts_of_speech))] #append definitions of part of speech from json
+        # append rows from csv file based on input word
+        dictionary = [row for row in r if row[1].lower().startswith(word.lower())]
+        # append definitions from dictionary
+        mal_definitions = [row[-1] for row in dictionary if row[1].lower().startswith(word.lower())]
+        # append english words from dictionary
+        eng_word = [row[1] for row in dictionary if row[1].lower().startswith(word.lower())]
+        # append parts of speech from dictionary
+        parts_of_speech = [row[-2] for row in dictionary if row[1].lower().startswith(word.lower())]
+        # append definitions of part of speech from json
+        parts_of_speech_defs = [parts_of_speech_json[parts_of_speech[p]]['en'] for p in range(0, len(parts_of_speech))]
 
     return mal_definitions, eng_word, parts_of_speech_defs
 
@@ -20,33 +26,55 @@ def malayalamDictBot(inword):
     word = inword.capitalize()
     mal_definitions, eng_word, parts_of_speech_defs = malayalamDict(word)
     if mal_definitions:
-        results = [f"● {str(eng_word[l])} - <b>{str(mal_definitions[l])}</b> {parts_of_speech_defs[l]}" for l in range(0, len(mal_definitions))] #formatting results
-        results = results[:20] #set limit for results list. pyrogram error 400-Bad Request, MESSAGE_TOO_LONG
+        # formatting results
+        results = [f"● {str(eng_word[l])} - <b>{str(mal_definitions[l])}</b> {parts_of_speech_defs[l]}" for l in range(0, len(mal_definitions))]
+        # set limit for results list. pyrogram error 400-Bad Request, MESSAGE_TOO_LONG
+        results = results[:20]
         return results
     else:
         results = [
             f'ക്ഷമിക്കുക, <b>"{inword}"</b> എന്ന വാക്കിന്‍റെ അര്‍ത്ഥം കണ്ടെത്താനായില്ല.']
         return results
 
-def inline_dict1(word):
-
+def inline_dict1(word, dlang):
+    c = Collator("data/allkeys.txt")
     with open("data/olam-enml.csv", "r") as f:
         r = csv.reader(f, delimiter='\t')
-        dictionary = [row for row in r if row[1].lower().startswith(word.lower())] #append rows from csv file based on input word
-        eng_word = [row[1] for row in dictionary if row[1].lower().startswith(word.lower())] #append english words from dictionary
-        eng_word = sorted(list(set(eng_word)), key=str.lower)[:20] #sorting alphabetically, removing duplicates and setting list limit (pyrogram error 400-Bad Request, RESULTS_TOO_MUCH).
+        #print(dictionary)
+        if dlang=="ml":
+            # append rows from csv file based on input word
+            dictionary = [row for row in r if row[-1].startswith(word)]
+            # append malayalam words from dictionary
+            words = [row[-1] for row in dictionary if row[-1].startswith(word)]
+            # sorting alphabetically, removing duplicates and setting list limit (pyrogram error 400-Bad Request, RESULTS_TOO_MUCH).
+            words = sorted(list(set(words)), key=c.sort_key)[:20]
+        else:
+            # append rows from csv file based on input word
+            dictionary = [row for row in r if row[1].lower().startswith(word.lower())]
+            # append english words from dictionary
+            words = [row[1] for row in dictionary if row[1].lower().startswith(word.lower())]
+            # sorting alphabetically, removing duplicates and setting list limit (pyrogram error 400-Bad Request, RESULTS_TOO_MUCH).
+            words = sorted(list(set(words)), key=str.lower)[:20]
 
-    return eng_word, dictionary
+    return words, dictionary
 
 def inline_dict2(word, dictionary):
 
+    #load json
     parts_of_speech_json = json.load(open("data/parts-of-speech.json"))
-    mal_definitions = [row[-1] for row in dictionary if word in row[1]] #append definitions from dictionary
-    eng_word = [row[1] for row in dictionary if word in row[1]] #append english words from dictionary
-    parts_of_speech = [row[-2] for row in dictionary if word in row[1]] #append parts of speech from dictionary
-    parts_of_speech_defs = [parts_of_speech_json[parts_of_speech[p]]['en'] for p in range(0, len(parts_of_speech))] #append definitions of part of speech from json
-    results_list = [f"● {str(eng_word[l])} - <b>{str(mal_definitions[l])}</b> {parts_of_speech_defs[l]}" for l in range(0, len(mal_definitions))]  #formatting results
-    results_limited = results_list[:20] #set limit for results list. pyrogram error 400-Bad Request, MESSAGE_TOO_LONG
-    results = f"\n".join(results_limited) #again formatting results
+    # append definitions from dictionary
+    mal_definitions = [row[-1] for row in dictionary if word in row[1] or row[-1].startswith(word)]
+    # append english words from dictionary
+    eng_word = [row[1] for row in dictionary if word in row[1] or row[-1].startswith(word)]
+    # append parts of speech from dictionary
+    parts_of_speech = [row[-2] for row in dictionary if word in row[1] or row[-1].startswith(word)]
+    # append definitions of part of speech from json
+    parts_of_speech_defs = [parts_of_speech_json[parts_of_speech[p]]['en'] for p in range(0, len(parts_of_speech))]
+    # formatting results
+    results_list = [f"● {str(eng_word[l])} - {str(mal_definitions[l])} {parts_of_speech_defs[l]}" for l in range(0, len(mal_definitions))]
+    # set limit for results list. pyrogram error 400-Bad Request, MESSAGE_TOO_LONG
+    results_limited = results_list[:20]
+    # again formatting results
+    results = f"\n".join(results_limited)
 
     return results
